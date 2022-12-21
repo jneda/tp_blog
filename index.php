fe0e5afe6a0cc5521672b16707dd7f43b48042a1
jdane <julien.dane@gmail.com>
2022-12-21 16:00:36 +0100

    Revert "feat: use layout for homepage"
    This reverts commit 7ffd4045ed584446aa22c46d37e907acd8bc2347.

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
 
-ob_start();
 echo '
   <h1>Bonjour tout le monde ! 😁</h1>
   <i><p>&copy; Un blog d\'Anthony Houlala</i></p>
 ';
-$content = ob_get_clean();
-$title = 'Bienvenue sur mon blog | Anthony Houlala';
-require './view/layout.php';
 
 ?>
