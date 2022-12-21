2ed12e2e2c038ca82aa53d5238665dccc0949fc2
jdane <julien.dane@gmail.com>
2022-12-21 16:40:14 +0100

    feat: add FontAwesome icons link

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 ob_start();
 require './view/template/hero.php';
+require './view/template/blogPosts.php';
 $content = ob_get_clean();
 $title = 'Bienvenue sur mon blog | Anthony Houlala';
 require './view/layout.php';
