d2857475cd108a6bc9971952f482d08e44f1ecbc
jneda <julien.dane@gmail.com>
2022-12-21 22:14:05 +0100

    feat: add footer

---
+++ view/template/footer.php
@@ -0,0 +1,6 @@
+<footer class="hero is-info is-small has-text-centered">
+  <div class="hero-body">
+    <h1 class="title">Au revoir tout le monde ! 😁</h1>
+    <p class="subtitle"><i>&copy; Un blog d'Anthony Houlala</i></p>
+  </div>
+</footer>
